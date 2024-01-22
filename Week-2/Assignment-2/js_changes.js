8154cf99b3f861b42ee4a4b5f637c8a6033679f2
beringsailor <44130540+beringsailor@users.noreply.github.com>
2024-01-22 16:45:05 +0800

    Update js_changes.js

--- Week-2/Assignment-2/js_changes.js
+++ Week-2/Assignment-2/js_changes.js
@@ -1,15 +1,11 @@
 const h2 = document.querySelector('h2');
-
 h2.addEventListener('click', () => {
   h2.textContent = "Have a Good Time!";
 });
 
-
 const callToAction = document.querySelector('div#button');
-
 callToAction.addEventListener('click', () => {
     const container2 = document.querySelector('div.container-2');
-
     if ( container2.style.display = 'none' ) {
         container2.style.display = "flex";
     }
