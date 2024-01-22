1bb640b7165313c4b05a3fc4aa665b8c64106b80
beringsailor <44130540+beringsailor@users.noreply.github.com>
2024-01-22 17:02:43 +0800

    Update js_changes.js

--- Week-2/Assignment-2/js_changes.js
+++ Week-2/Assignment-2/js_changes.js
@@ -4,10 +4,9 @@ welcomemsg.addEventListener('click', () => {
   welcomemsg.textContent = "Have a Good Time!";
 });
 
-// const callToAction = document.querySelector('div#button');
-// callToAction.addEventListener('click', () => {
-//     const container2 = document.querySelector('div.container-2');
-//     if ( container2.style.display = 'none' ) {
-//         container2.style.display = "flex";
-//     }
-// });
+
+const btnClick = document.querySelector('#button');
+btnClick.addEventListener('click', () => {
+    const container2 = document.querySelector('div.container-2');
+    container2.style.display = 'flex';
+});
